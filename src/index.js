a90c66ecf21792ec50d9adc827c05fbd7a0f6f04
Denny <Dennyng1989@hotmail.com>
2023-05-21 15:53:35 -0700

    Impl daylist component and logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
+import DayListItem from "components/DayListItem";
 
 import Application from "components/Application";
 
