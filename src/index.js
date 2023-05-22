256ee8feb85a79390965ce206903f738f869e2f6
Denny <Dennyng1989@hotmail.com>
2023-05-21 23:16:36 -0700

    Delete erroneously added code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
-import DayListItem from "components/DayListItem";
 
 import Application from "components/Application";
 
