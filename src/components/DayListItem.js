73b47d4124ad006e4afcc4811a3d25f68f017967
Denny <Dennyng1989@hotmail.com>
2023-05-21 21:43:07 -0700

    Impl formatting for remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,24 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = (spots) => {
+    let formattedSpots;
+
+    if(spots === 0) {formattedSpots = 'no spots'};
+    if(spots === 1) {formattedSpots = '1 spot'};
+    if(spots > 1) {formattedSpots = `${spots} spots`};
+
+    return formattedSpots;
+  }
+  
+
   return (
     <li 
       className={dayClass} 
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
