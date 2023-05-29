f92da696742ed09de4c143a1abcd63bc2a9cf291
Denny <Dennyng1989@hotmail.com>
2023-05-28 20:59:07 -0700

    Impl. Form component

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 storiesOf("Button", module)
   .addParameters({
@@ -187,5 +188,22 @@ storiesOf("InterviewerListItem", module)
       <Error
         message="Could not delete appointment"
         onClose={action("onClose")}
+      />
+    ))
+    .add("Create", () => (
+      <Form 
+        interviewers={interviewers}
+
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Edit", () => (
+      <Form 
+        student="John Doe"
+        interviewer={2}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
       />
     ))
