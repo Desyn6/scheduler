5546203e1268884d5ff2992a42f2c64c94232977
Denny <Dennyng1989@hotmail.com>
2023-05-30 22:36:32 -0700

    Aggregate day, days, appts into single useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,12 +46,18 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+    // ,
+    // appointments: {}
+  })
 
-  // fetch days data from API server using setDays+useEffect
-  const [days, setDays] = useState([]);
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
+
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
+    axios.get("/api/days")
       .then((res) => setDays(res.data))
   }, []);
 
@@ -66,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
