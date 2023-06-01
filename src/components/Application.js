5bfa4a43ea91928a83f692a2eb213a621c43273c
Denny <Dennyng1989@hotmail.com>
2023-05-31 19:23:25 -0700

    Impl. dynamically fetched interviews

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,14 +4,15 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   // declare combinated useState
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    inverviewers: {}
   })
 
   // click handler for setting day
@@ -23,12 +24,15 @@ export default function Application(props) {
   // fetch days and appointment data from API
   Promise.all([
     axios.get('/api/days'),
-    axios.get('/api/appointments')
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers')
   ]).then((all) => {
     setState(state => ({
       ...state, 
       days: all[0].data, 
-      appointments: all[1].data}))
+      appointments: all[1].data,
+      interviewers: all[2].data
+    }))
   });
 
   return (
@@ -57,10 +61,13 @@ export default function Application(props) {
         { 
 
           dailyAppointments.map((appointment) => {
+            const interview = getInterview(state, appointment.interview)
             return (
               <Appointment 
                 key={appointment.id}
-                {...appointment}
+                id={appointment.id}
+                time={appointment.time}
+                interview={interview}
               />
             )
           })
