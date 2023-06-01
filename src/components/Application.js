1324ee56da8c492f6b638f9dfef1a2fe22fe58cc
Denny <Dennyng1989@hotmail.com>
2023-06-01 00:55:53 -0700

    Impl. getInterviewersForDay for fetched data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
   // declare combinated useState
@@ -18,7 +18,7 @@ export default function Application(props) {
   // click handler for setting day
   const setDay = day => setState({...state, day});
 
-  // write appointment selected day
+  // write appointment for selected day
   const dailyAppointments = getAppointmentsForDay(state, state.day)
 
   // fetch days and appointment data from API
@@ -59,15 +59,17 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         { 
-
           dailyAppointments.map((appointment) => {
-            const interview = getInterview(state, appointment.interview)
+            const interview = getInterview(state, appointment.interview);
+            const interviewers = getInterviewersForDay(state, state.day);
+
             return (
               <Appointment 
                 key={appointment.id}
                 id={appointment.id}
                 time={appointment.time}
                 interview={interview}
+                interviewers={interviewers}
               />
             )
           })
