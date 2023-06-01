a033c01b3b64269873831afa468ab51d651f7b41
Denny <Dennyng1989@hotmail.com>
2023-06-01 00:40:24 -0700

    Impl. getInterviewersForDay fn

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -25,3 +25,22 @@ export function getInterview(state, interview) {
     interviewer: state.interviewers[interview.interviewer]
   }  
 }
+
+export function getInterviewersForDay(state, day) {
+  //... returns an array of appointments for that day
+  const days = state.days;
+  const appointments = state.appointments;
+  const interviewers = state.interviewers;
+  const results = [];
+
+  //iterate through days to search for match with day
+  for (const el of days) {
+    if (el.name === day) {
+      //iterate through days.appointment (interview number)
+      //push corresponding appointment number from appointments object
+      el.interviewers.forEach((itvNum) => results.push(interviewers[itvNum]))
+    }
+  }
+  console.log(results)
+  return results;
+}
